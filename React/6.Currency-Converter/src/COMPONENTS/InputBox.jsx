424cb6888682f8cbc6653ae82244b7c5a0d5b009
Abhinav Kumar Yadav <abhinavk1608@gmail.com>
2025-06-13 13:34:52 +0530

    "changed bg color"

--- React/6.Currency-Converter/src/COMPONENTS/InputBox.jsx
+++ React/6.Currency-Converter/src/COMPONENTS/InputBox.jsx
@@ -8,7 +8,7 @@ function InputBox({ label, amount, onAmountChange,  currencyOptions=[],onCurrenc
    
 
     return (
-        <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
+        <div className={`bg-gray-500 p-3 rounded-lg text-sm flex ${className}`}>
             <div className="w-1/2">
                 <label htmlFor={amountInputId} className="text-black/40 mb-2 inline-block">
                     {label}
