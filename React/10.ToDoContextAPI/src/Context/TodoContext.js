493a87132dfb5245812fc9e456b2dbe263c6cc98
Abhinav Kumar Yadav <abhinavk1608@gmail.com>
2025-06-28 15:18:06 +0530

    .

--- React/10.ToDoContextAPI/src/Context/TodoContext.js
+++ React/10.ToDoContextAPI/src/Context/TodoContext.js
@@ -1,21 +1,22 @@
 import  { createContext,useContext } from 'react'
 
-
-
-export const TodoContext= createContext({
+export const TodoContext= createContext(
+  {
   todos:[
   {
     id:1,
     todo:"new todo",
     completed:false
-  }],
+  }
+],
 
   addTodo:(todo) =>{},
   updateTodo:(id,todo) =>{},
   deleteTodo:(id) =>{},
   togleComplete:(id) =>{}
 
-});
+}
+);
  
 export const useTodo= () => {
   return useContext(TodoContext)
